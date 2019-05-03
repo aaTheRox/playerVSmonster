12bdd96f61fd2e442673293a4cd3d326f21599c9
nouaryk <nouary@live.com>
2019-05-03 15:12:55 +0200

    404 page not found

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import FormNickname from './views/FormNickname.vue'
+import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
 
@@ -14,6 +15,8 @@ export default new Router({
             component: Home,
             FormNickname
         },
+        { path: '/404', component: NotFound },
+        { path: '*', redirect: '/404' },
         {
             path: '/about',
             name: 'about',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="title">Page not found !</h1>
+    <img src="https://i.pinimg.com/originals/3a/11/68/3a116884945f870924f1ffd3f36fc015.png" alt="">
+  </div>
+</template>
